dad6631db0bf4e18a21512876cbb41d913d01e78
zessia <88737296+zessia@users.noreply.github.com>
2021-08-20 19:13:18 +0200

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>php inscription et connexion</title>
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+
+<body>
+	<div class="form-inscription">
+	    <div class="form-text">INSCRIPTION</div>
+
+	    <div class="form-saisie">
+	    	<form method="" action="">  
+
+	    		<span>NOM & PRENOM :</span>
+	    		<input type="text" name="" placeholder="saisissez votre nom" required=""/>
+
+	    		<span>Adress Email :</span>
+	    		<input type="Email" name="" placeholder="saisissez votre Email" required=""/>
+
+	    		<span>Mot de passe :</span>
+	    		<input id="userPassword" type="password"; type="passe" name="" placeholder="saisissez votre Mot de passe" required="";/>
+
+				<div class="button">
+
+                 <button type="submit" id="search" value="search" style="height:40px; width:290px" ><a href="file:///C:/Users/Pacome%20NGOUAN/Desktop/doss/cv/boncv.html">s'inscrire</a></button> 
+                 </div>
+	    		
+	    		 <span>vous êtes déjà inscrit ?</span>
+	    		 <a href="file:///C:/Users/Pacome%20NGOUAN/Desktop/formulaire_php/connexion/connexion.html"style="text-decoration: none;">connectez-vous</a>
+
+	    		
+	    		<!-- <!--   --<!-- ><a href="/" style="text-decoration: none;" > connectez-vous</a> -->
+	    				 
+
+	    	</form>
+	</div>
+
+</body>
+</html>
